90111be2ce3abb71e5b86dcbb85c09171169a082
finestQ-alex <finestq_alex@icloud.com>
2024-12-12 23:15:41 +0900

    svelte, vite로 바꾼 후 firebase

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Main from "./pages/Main.svelte";
+  import Login from "./pages/Login.svelte";
+  import Write from "./pages/Write.svelte";
+  import Singup from "./pages/Singup.svelte";
+  import Router from "svelte-spa-router";
+  import NotFound from "./pages/NotFound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Singup,
+    "/write": Write,
+    "*": NotFound,
+  };
+</script>
+
+<Router {routes} />
